1881309a3ca98e265d3b29f7d15b65f76a81a666
Anderson Meireles <anderson_meireles@id.uff.br>
2022-09-07 12:03:41 -0300

    done

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $("#header").load("../components/header.html"); 
+  $("#footer").load("../components/footer.html"); 
+});
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
