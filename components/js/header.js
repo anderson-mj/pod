acd41b4a3d0664c6d80f720b49fce412e35cd66e
Anderson Meireles <anderson_meireles@id.uff.br>
2022-09-11 15:49:02 -0300

    adding form email sending

--- components/js/header.js
+++ components/js/header.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.matricula').mask('000.000.000');
     $('.phone').mask('(00) 00000-0000');
 });
@@ -6,3 +6,18 @@ $(document).ready(function(){
 function clearMessage() {
     $('#message-text').val('');
 }
+
+var form = document.getElementById("suggestion-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+}
+form.addEventListener("submit", handleSubmit)
